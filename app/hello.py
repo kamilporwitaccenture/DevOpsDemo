5ff195244b2ea8f769acc6fb05485dca5e44a58e
Kamil Porwit <kamil.porwit@accenture.com>
2019-11-19 13:11:50 +0100

    test

--- app/hello.py
+++ app/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Docker container"
+    return "Hello World from Docker container test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port= 4000)
