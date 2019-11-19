174be1897a90f96559a80e165c142b9ba009fc8f
Kamil Porwit <kamil.porwit@accenture.com>
2019-11-19 12:38:36 +0100

    final test of build triggers

--- app/hello.py
+++ app/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World TEST TEST2"
+    return "Hello World final test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port= 4000)
