75eb85a3eda48abf0895563dafd741cc5b6cf2a4
Kamil Porwit <kamil.porwit@accenture.com>
2019-11-21 07:38:26 +0100

    test of deployment2

--- app/hello.py
+++ app/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Docker TEST"
+    return "Hello World from Docker TEST TEST2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port= 4000)
